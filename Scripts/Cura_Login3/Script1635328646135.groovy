7834036cd89c73156a1d04cf47a63121c2b46b24
izzulm <muhizzul16@gmail.com>
2021-11-05 10:35:44 +0700

    first commit

---
+++ Scripts/Cura_Login3/Script1635328646135.groovy
@@ -0,0 +1,62 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.waitForPageLoad(5)
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/i_CURA Healthcare_fa fa-bars'), 
+    0)
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/i_CURA Healthcare_fa fa-bars'))
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/a_Login'))
+
+//looping
+for(def rowNum = 1; rowNum <= findTestData("Data1").getRowNumbers(); rowNum++ ) {
+	
+	WebUI.setText(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/input_Username_username'), findTestData("Data1").getValue(1, 1))
+	
+	WebUI.setText(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/input_Password_password'), findTestData("Data1").getValue(2, 1))
+	
+	delay(3)
+	}
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.waitForPageLoad(5)
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/input_Apply for hospital readmission_hospit_63901f'))
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/span_Visit Date (Required)_glyphicon glyphi_cada34'))
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/td_11'))
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/td_11'))
+
+WebUI.setText(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/textarea_Comment_comment'), 'Good Mood Good News')
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.click(findTestObject('Object Repository/Cura_Test01/Page_CURA Healthcare Service/a_Go to Homepage'))
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Cek TestSuite.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>update testsuite</description>
+   <name>Cek TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e162d99f-7f0f-40fa-8858-eff1556d4b77</testSuiteGuid>
+   <testCaseLink>
+      <guid>9fbb878c-b8ce-4cbd-a4ef-df307869d8d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cura_Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>858c6dc8-bac4-404a-be01-e63e27ae007c</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>858c6dc8-bac4-404a-be01-e63e27ae007c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>102acd07-cdf9-4712-9046-31d31cb6319d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>858c6dc8-bac4-404a-be01-e63e27ae007c</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>39e7155c-a35e-4e4c-a15a-53e84193819b</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c6f31e4-85d9-4860-9859-70fb6d50bff9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cura_Login4</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>06426afe-cb8a-4685-ad98-8d59bef155a3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>75a45b8e-a80e-42db-8e19-36a4938fea59</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
